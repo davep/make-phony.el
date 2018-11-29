70e4803ccc82f30ac909f3d805a3342d06a61242
Dave Pearson <davep@davep.org>
2018-11-29 21:49:25 +0000

    :bookmark: v1.1

--- make-phony.el
+++ make-phony.el
@@ -2,7 +2,7 @@
 ;; Copyright 2018 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.00
+;; Version: 1.1
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/make-phony.el
 ;; Package-Requires: ((emacs "24"))
